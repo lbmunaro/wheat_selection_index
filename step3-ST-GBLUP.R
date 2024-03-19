ac471a6d723981607c5dda3302fd4cad9e7ab6c9
lbmunaro <lucasb4@illinois.edu>
2024-03-19 13:07:12 -0500

    Single-trait GBLUP

--- step3-ST-GBLUP.R
+++ step3-ST-GBLUP.R
@@ -10,11 +10,10 @@ rm(list=objects()) # clean workspace
 # Packages ----
 library(tidyverse) # R packages for data science
 library(asreml) # ASReml-R package
-asreml.options(maxit= 150)
 
 # Load data ----
 ## BLUES & K2 ----
-load("data/step2-subBLUES_K2.RData")
+load('data/step2-subBLUES_K2.RData')
 
 # ST-GBLUP ----
 
@@ -28,9 +27,9 @@ mod_gy_22 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22 |> filter(trait=="grain_yield"), 
+                    data=bluesG_22 |> filter(trait=='grain_yield'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_gy_22 <- predict(mod_gy_22, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -48,9 +47,9 @@ mod_tw_22 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22 |> filter(trait=="test_weight"), 
+                    data=bluesG_22 |> filter(trait=='test_weight'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_tw_22 <- predict(mod_tw_22, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -68,9 +67,9 @@ mod_ht_22 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22 |> filter(trait=="heading_time"), 
+                    data=bluesG_22 |> filter(trait=='heading_time'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_ht_22 <- predict(mod_ht_22, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -88,9 +87,9 @@ mod_ph_22 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22 |> filter(trait=="plant_height"), 
+                    data=bluesG_22 |> filter(trait=='plant_height'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_ph_22 <- predict(mod_ph_22, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -105,13 +104,13 @@ GEBV_ph_22 <- predict(mod_ph_22, classify='germplasm', ignore=c('trial'), pworks
 ### GEBVs ----
 # 2022 GEBVs - single-trait GBLUP
 GEBVs_ST_GBLUP_22 <- data.frame() |>
-  bind_rows(GEBV_gy_22 |> mutate(trait="grain_yield"),
-            GEBV_tw_22 |> mutate(trait="test_weight"),
-            GEBV_ht_22 |> mutate(trait="heading_time"),
-            GEBV_ph_22 |> mutate(trait="plant_height")) |>
+  bind_rows(GEBV_gy_22 |> mutate(trait='grain_yield'),
+            GEBV_tw_22 |> mutate(trait='test_weight'),
+            GEBV_ht_22 |> mutate(trait='heading_time'),
+            GEBV_ph_22 |> mutate(trait='plant_height')) |>
   glimpse()
 # remove individual GEBV files
-rm("GEBV_gy_22", "GEBV_tw_22", "GEBV_ht_22", "GEBV_ph_22")
+rm('GEBV_gy_22', 'GEBV_tw_22', 'GEBV_ht_22', 'GEBV_ph_22')
 
 ## 2023 ----
 bluesG_23 |> glimpse()
@@ -123,9 +122,9 @@ mod_gy_23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_23 |> filter(trait=="grain_yield"), 
+                    data=bluesG_23 |> filter(trait=='grain_yield'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_gy_23 <- predict(mod_gy_23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -143,9 +142,9 @@ mod_tw_23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_23 |> filter(trait=="test_weight"), 
+                    data=bluesG_23 |> filter(trait=='test_weight'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_tw_23 <- predict(mod_tw_23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -163,9 +162,9 @@ mod_ht_23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_23 |> filter(trait=="heading_time"), 
+                    data=bluesG_23 |> filter(trait=='heading_time'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_ht_23 <- predict(mod_ht_23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -183,9 +182,9 @@ mod_ph_23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_23 |> filter(trait=="plant_height"), 
+                    data=bluesG_23 |> filter(trait=='plant_height'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_ph_23 <- predict(mod_ph_23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -200,13 +199,13 @@ GEBV_ph_23 <- predict(mod_ph_23, classify='germplasm', ignore=c('trial'), pworks
 ### GEBVs ----
 # 2023 GEBVs - single-trait GBLUP
 GEBVs_ST_GBLUP_23 <- data.frame() |>
-  bind_rows(GEBV_gy_23 |> mutate(trait="grain_yield"),
-            GEBV_tw_23 |> mutate(trait="test_weight"),
-            GEBV_ht_23 |> mutate(trait="heading_time"),
-            GEBV_ph_23 |> mutate(trait="plant_height")) |>
+  bind_rows(GEBV_gy_23 |> mutate(trait='grain_yield'),
+            GEBV_tw_23 |> mutate(trait='test_weight'),
+            GEBV_ht_23 |> mutate(trait='heading_time'),
+            GEBV_ph_23 |> mutate(trait='plant_height')) |>
   glimpse()
 # remove individual GEBV files
-rm("GEBV_gy_23", "GEBV_tw_23", "GEBV_ht_23", "GEBV_ph_23")
+rm('GEBV_gy_23', 'GEBV_tw_23', 'GEBV_ht_23', 'GEBV_ph_23')
 
 ## 2022 & 2023 ----
 bluesG_22.23 |> glimpse()
@@ -218,9 +217,9 @@ mod_gy_22.23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22.23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22.23 |> filter(trait=="grain_yield"), 
+                    data=bluesG_22.23 |> filter(trait=='grain_yield'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_gy_22.23 <- predict(mod_gy_22.23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -239,9 +238,9 @@ mod_tw_22.23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22.23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22.23 |> filter(trait=="test_weight"), 
+                    data=bluesG_22.23 |> filter(trait=='test_weight'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_tw_22.23 <- predict(mod_tw_22.23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -260,9 +259,9 @@ mod_ht_22.23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22.23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22.23 |> filter(trait=="heading_time"), 
+                    data=bluesG_22.23 |> filter(trait=='heading_time'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_ht_22.23 <- predict(mod_ht_22.23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -281,9 +280,9 @@ mod_ph_22.23 <- asreml(fixed=predicted.value~1,
                     random=~vm(germplasm, K_22.23)+trial,
                     weights=weight,
                     family=asr_gaussian(dispersion = 1),
-                    data=bluesG_22.23 |> filter(trait=="plant_height"), 
+                    data=bluesG_22.23 |> filter(trait=='plant_height'), 
                     na.action = na.method(y='include', x='include'),
-                    workspace="8gb")
+                    workspace='8gb', maxit= 50)
 #gebvs
 GEBV_ph_22.23 <- predict(mod_ph_22.23, classify='germplasm', ignore=c('trial'), pworkspace='8gb')$pvals |>
   as.data.frame() |>
@@ -299,34 +298,34 @@ GEBV_ph_22.23 <- predict(mod_ph_22.23, classify='germplasm', ignore=c('trial'), 
 ### GEBVs ----
 # 2023 GEBVs - single-trait GBLUP
 GEBVs_ST_GBLUP_22.23 <- data.frame() |>
-  bind_rows(GEBV_gy_22.23 |> mutate(trait="grain_yield"),
-            GEBV_tw_22.23 |> mutate(trait="test_weight"),
-            GEBV_ht_22.23 |> mutate(trait="heading_time"),
-            GEBV_ph_22.23 |> mutate(trait="plant_height")) |>
+  bind_rows(GEBV_gy_22.23 |> mutate(trait='grain_yield'),
+            GEBV_tw_22.23 |> mutate(trait='test_weight'),
+            GEBV_ht_22.23 |> mutate(trait='heading_time'),
+            GEBV_ph_22.23 |> mutate(trait='plant_height')) |>
   glimpse()
 # remove individual GEBV files
-rm("GEBV_gy_22.23", "GEBV_tw_22.23", "GEBV_ht_22.23", "GEBV_ph_22.23")
+rm('GEBV_gy_22.23', 'GEBV_tw_22.23', 'GEBV_ht_22.23', 'GEBV_ph_22.23')
 
 # Plot models ----
 # Create a loop to save all models plots in a separate folder (ST-GBLUP mod) with file name referring to the model
-object_names <- ls(pattern = "^mod_")
+object_names <- ls(pattern = '^mod_')
 
 # Create a function to save plots
 save_plot <- function(data, obj_name) {
   # Apply the plot() function
   plot(data)  # Adjust this line according to your plot function
   # Save the plot with the same name as the object in the specified folder
-  plot_dir <- "~/Documents/wheat_selection_index/plot_mod/ST-GBLUP/"  # Update this path with the absolute path
+  plot_dir <- '~/Documents/wheat_selection_index/plot_mod/ST-GBLUP/'  # Update this path with the absolute path
   # Create the directory if it doesn't exist
   if (!dir.exists(plot_dir)) {
     if (!dir.create(plot_dir, recursive = TRUE)) {
-      stop("Failed to create directory:", plot_dir)
+      stop('Failed to create directory:', plot_dir)
     }
   }
-  plot_name <- paste0(plot_dir, obj_name, ".png")
+  plot_name <- paste0(plot_dir, obj_name, '.png')
   # Save the plot as a PNG file
   if (!try(png(plot_name), silent = TRUE)) {
-    stop("Failed to save plot:", plot_name)
+    stop('Failed to save plot:', plot_name)
   }
   dev.off()
 }
@@ -340,7 +339,7 @@ for (obj_name in object_names) {
 }
 
 # Save ----
-rm(list=c(ls(pattern = "^bluesG_"), ls(pattern = "^K_"), ls(pattern = "^check_"),
-          "object_names", "obj_name", "save_plot", "data"))
+rm(list=c(ls(pattern = '^bluesG_'), ls(pattern = '^K_'), ls(pattern = '^check_'),
+          'object_names', 'obj_name', 'save_plot', 'data'))
 
-save.image("data/step3-ST-GBLUP.RData")
+save.image('data/step3-ST-GBLUP.RData')
