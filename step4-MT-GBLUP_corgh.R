eedf4d41eb9a0c4afae3e08dcb3cc0796292c82a
lbmunaro <lucasb4@illinois.edu>
2024-03-19 13:22:09 -0500

    MT-GBLUP - corgh()

---
+++ step4-MT-GBLUP_corgh.R
@@ -0,0 +1,120 @@
+# Multi-trait GBLUP ----
+
+# Objective ----
+# - Run Multi-trait GBLUP for each scenario using corgh()
+# - Get GEBVs
+# - Calculate reliability of each model
+
+# Notes ----
+# Note that the argument na.action = na.method(x = "include", y = "include") can be added to the model. In a bivariate model, it will help calculate the covariance between two traits with different missing information NA and so help imbalance phenotypage and save sample size. However, it is important to scale ( mean =0, var =1) the two traits to correctly adjust the model(see Asreml-R manual for more information).
+
+# https://juliengamartin.github.io/wam_tuto/asreml-biv.html
+
+rm(list=objects()) # clean workspace
+
+# Packages ----
+
+library(tidyverse) # R packages for data science
+library(asreml) # ASReml-R package
+
+# Load data ----
+## BLUES & K2 ----
+load('data/step2-subBLUES_K2.RData')
+
+# MT-GBLUP ----
+
+## 2022 ----
+bluesG_22 <- bluesG_22 |> 
+  filter(trait!='maturity') |> 
+  droplevels() |>
+  glimpse()
+K_22 |> glimpse()
+
+#model
+mod_MT_GBLUP_22 <- asreml(fixed = predicted.value~trait,
+                          random = ~corgh(trait):vm(germplasm, K_22) +
+                            us(trait):trial,
+                          weights = weight, data = bluesG_22,
+                          family=asr_gaussian(dispersion = 1),
+                          na.action = na.method(y='include', x='include'),
+                          workspace = '32gb', maxit = 50)
+GEBVs_MT_GBLUP_22 <- update(GEBVs_MT_GBLUP_22)
+GEBVs_MT_GBLUP_22 <- predict(mod_MT_GBLUP_22, classify='germplasm:trait', ignore=c('trial'),
+                             pworkspace='16gb')$pvals
+
+## 2023 ----
+bluesG_23 <- bluesG_23 |> 
+  filter(trait!='maturity') |> 
+  droplevels() |>
+  glimpse()
+K_23 |> glimpse()
+
+#model
+mod_MT_GBLUP_23 <- asreml(fixed = predicted.value~trait,
+                          random = ~corgh(trait):vm(germplasm, K_23) + 
+                            us(trait):trial,
+                          weights = weight, data = bluesG_23,
+                          family=asr_gaussian(dispersion = 1),
+                          na.action = na.method(y='include', x='include'),
+                          workspace = '32gb', maxit = 50)
+GEBVs_MT_GBLUP_23 <- update(GEBVs_MT_GBLUP_23)
+GEBVs_MT_GBLUP_23 <- predict(mod_MT_GBLUP_23, classify='germplasm:trait', ignore=c('trial'),
+                             pworkspace='16gb')$pvals
+
+## 2022 & 2023 ----
+bluesG_22.23 <- bluesG_22.23 |> 
+  filter(trait!='maturity') |> 
+  droplevels() |>
+  glimpse()
+K_22.23 |> glimpse()
+
+#model
+mod_MT_GBLUP_22.23 <- asreml(fixed = predicted.value~trait,
+                             random = ~corgh(trait):vm(germplasm, K_22.23) + 
+                               us(trait):trial,
+                             weights = weight, data = bluesG_22.23,
+                             family=asr_gaussian(dispersion = 1),
+                             na.action = na.method(y='include', x='include'),
+                             workspace = '32gb', maxit = 50)
+mod_MT_GBLUP_22.23 <- update(mod_MT_GBLUP_22.23)
+GEBVs_MT_GBLUP_22.23 <- predict(mod_MT_GBLUP_22.23, classify='germplasm:trait', ignore=c('trial'),
+                                pworkspace='16gb')$pvals
+
+# Plot models ----
+
+# Create a loop to save all models plots in a separate folder (MT-GBLUP mod) with file name referring to the model
+object_names <- ls(pattern = '^mod_')
+
+# Create a function to save plots
+save_plot <- function(data, obj_name) {
+  # Apply the plot() function
+  plot(data)  # Adjust this line according to your plot function
+  # Save the plot with the same name as the object in the specified folder
+  plot_dir <- '~/Documents/wheat_selection_index/plot_mod/MT-GBLUP/'  # Update this path with the absolute path
+  # Create the directory if it doesn't exist
+  if (!dir.exists(plot_dir)) {
+    if (!dir.create(plot_dir, recursive = TRUE)) {
+      stop('Failed to create directory:', plot_dir)
+    }
+  }
+  plot_name <- paste0(plot_dir, obj_name, '.png')
+  # Save the plot as a PNG file
+  if (!try(png(plot_name), silent = TRUE)) {
+    stop('Failed to save plot:', plot_name)
+  }
+  dev.off()
+}
+
+# Iterate over each object
+for (obj_name in object_names) {
+  # Extract the object
+  data <- get(obj_name)
+  # Save the plot
+  try(save_plot(data, obj_name), silent = TRUE)
+}
+
+# Save ----
+rm(list=c(ls(pattern = '^bluesG_'), ls(pattern = '^K_'), ls(pattern = '^check_'),
+          'object_names', 'obj_name', 'save_plot', 'data'))
+save.image('data/step4-MT-GBLUP_corgh.RData')
+#load('data/step4-MT-GBLUP_corgh.RData')
